9a3b994fceb68e3678498c8cbc8a39ede928daa8
Mithila30 <mithilamuthyam@gmail.com>
2025-03-05 23:41:28 +0530

    commit all get post users

--- src/main/java/com/learnspringboot/rest/webservices/restful_web_services/User/UserResoource.java
+++
@@ -1,8 +0,0 @@
-package com.learnspringboot.rest.webservices.restful_web_services.User;
-
-import org.springframework.web.bind.annotation.RestController;
-
-@RestController
-public class UserResoource {
-
-}
